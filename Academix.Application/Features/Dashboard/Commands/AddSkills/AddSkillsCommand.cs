ffef68c61156102e18f15e25327250d1ee499cac
HosamGirgis55 <hosamgirgis55@gmail.com>
2025-07-15 15:34:42 +0300

    edit add skill

--- Academix.Application/Features/Dashboard/Commands/AddSkills/AddSkillsCommand.cs
+++ Academix.Application/Features/Dashboard/Commands/AddSkills/AddSkillsCommand.cs
@@ -10,7 +10,6 @@ namespace Academix.Application.Features.Dashboard.Commands.AddSkills
 {
     public class AddSkillsCommand : IRequest<Result>
     {
-        public string NameAr { get; set; } = string.Empty;
         public string NameEn { get; set; } = string.Empty;
     }
 }
